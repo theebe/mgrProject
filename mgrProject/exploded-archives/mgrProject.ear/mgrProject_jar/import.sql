c11dd3196231f515997762440d67b65d1087dbed
Szymon Skupien <alomoc@gmail.com>
2011-07-12 21:37:47 +0200

    czyszczenie projektu

--- mgrProject/exploded-archives/mgrProject.ear/mgrProject_jar/import.sql
+++
@@ -1,3 +0,0 @@
--- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
-
-insert into users (id, username, password, name) values (0, 'admin', 'admin', 'Administrator');
