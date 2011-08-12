c06569eaf4589e27073f7afd8bfbdfa84950ad91
Bartlomiej Wojas <wartek2@gmail.com>
2011-08-12 15:48:52 +0200

    Wstepna implementacja algorytmu tworzenia macierzy sasiedztwa.

--- mgrProject/src/hot/pl/mgrProject/action/AlgorithmBean.java
+++ mgrProject/src/hot/pl/mgrProject/action/AlgorithmBean.java
@@ -38,14 +38,16 @@ public class AlgorithmBean implements Algorithm {
 		
 		List<PrzystanekTabliczka> tabForStart = mgrDatabase.createNamedQuery("tabliczniPoPrzystanku").setParameter("przystanek", pstart).getResultList();
 		List<PrzystanekTabliczka> tabForStop = mgrDatabase.createNamedQuery("tabliczniPoPrzystanku").setParameter("przystanek", pstop).getResultList();
-
 		log.info("Liczba tabliczek dla start: " + tabForStart.size());
+		log.info("Liczba tabliczek dla stop: " + tabForStop.size());
 			//pobieranie pierwszej tabliczki z listy dla danego przystanku pocz¹tkowego
 			//na razie nie wiadomo dla której tabliczki mozna znalezc krotsza trase.
 			//bedzie sie trzeba nad tym jeszcze zastanowic
-		Long startID = tabForStart.get(0).getId();
-		log.info("Liczba tabliczek dla stop: " + tabForStop.size());
-		Long stopID  = tabForStop.get(0).getId();;
+		PrzystanekTabliczka tabStart = tabForStart.get(0);
+		PrzystanekTabliczka tabStop  = tabForStop.get(0);
+
+		Long startID = tabStart.getId();
+		Long stopID  = tabStop.getId();;
 		int start = -1;
 		int stop  = -1;
 		int inf = 1000; //nieskonczonosc. Oznacza brak krawedzi miedzy wierzcholkami.
@@ -56,14 +58,8 @@ public class AlgorithmBean implements Algorithm {
 		int n = tabliczki.size();
 		
 		//odwzorowanie przystanku na indeks tablicy poniewaz algorytm operuje na indeksach tablicy
-		for (int i = 0; i < n; ++i) {
-			if (tabliczki.get(i).getId() == startID) {
-				start = i;
-			}
-			if (tabliczki.get(i).getId() == stopID) {
-				stop = i;
-			}
-		}
+		start = getIndex(startID, tabliczki);
+		stop  = getIndex(stopID,  tabliczki);
 		
 		int[][] E = new int[n][n];  //macierz sasiedztwa
 		Integer[] V = new Integer[n]; //wektor wierzcholkow
@@ -75,6 +71,85 @@ public class AlgorithmBean implements Algorithm {
 			}
 			V[i] = i;
 		}
+		
+		List<Integer> tabSprawdzone = new ArrayList<Integer>();
+		//tabSprawdzone.add(start);
+		List<Integer> tabTrace = new ArrayList<Integer>();
+		
+		//Algorytm budowania macierzy sasiedztwa. Na razie wagi krawedzi przypisane sa na sztywno.
+		//Po testach wyglada na to, ze dziala poprawnie. Nie uwzglednia przystankow ktore nie sa dodane do zadnej linii.
+		//TODO: Przemyslec dokladniej przypadki kiedy ostatni przystanek(tabliczka) linii nie ma nastepnego.
+		//      Algorytm wymaga aby kazdy wierzholek mial przynajmniej jedna krawedz wychodzaca.
+		//Propozycja: Ostatnia tabliczka linii ma krawedz wychodzaca wskazujaca na ta siebie sama.
+		int aktualny = start, nastepny = -1;
+		boolean backTrace = false;
+		while (tabSprawdzone.size() <= tabliczki.size()) {
+			try {
+				nastepny = getIndex(tabliczki.get(aktualny).getNastepnyPrzystanek().getId(), tabliczki);
+				if (tabSprawdzone.contains(nastepny)) {
+					E[aktualny][nastepny] = 10;
+					log.info("[A]Laczenie: " + tabliczki.get(aktualny).getId() + ":" + tabliczki.get(nastepny).getId());
+					tabTrace.add(aktualny);
+					aktualny = nastepny;
+					backTrace = true;
+				} else {
+					log.info("Nastepny: " + tabliczki.get(nastepny).getId());
+					E[aktualny][nastepny] = 10;
+					log.info("[B]Laczenie: " + tabliczki.get(aktualny).getId() + ":" + tabliczki.get(nastepny).getId());
+
+					tabSprawdzone.add(nastepny);
+					tabTrace.add(aktualny);
+					aktualny = nastepny;
+					
+					log.info("TabTrace add " + tabliczki.get(aktualny).getId());
+					log.info("Koniec petli: " + (tabSprawdzone.size() <= tabliczki.size()));
+				}
+			} catch(Exception e) {
+				log.info("Nie ma nastepnego");
+				backTrace = true;
+			} finally {
+				if (backTrace) {
+					for (int i = tabTrace.size()-1; i >= 0; --i) {
+						nastepny = getIndex(tabliczki.get(tabTrace.get(i)).getId(), tabliczki);
+						log.info("Nastepny: " + tabliczki.get(nastepny).getId());
+						E[aktualny][nastepny] = 10;
+						log.info("[D]Laczenie: " + tabliczki.get(aktualny).getId() + ":" + tabliczki.get(nastepny).getId());
+						aktualny = nastepny;
+					}
+					tabTrace.clear();
+					log.info("TabTrace clear");
+					backTrace = false;
+					//==============
+					for (int i = 0; i < tabliczki.size(); ++i) {
+						if (!tabSprawdzone.contains(i)) {
+							aktualny = i;
+							tabSprawdzone.add(aktualny);
+							break;
+						} else {
+							aktualny = -1;
+						}
+					}
+					//dzieki temu petla while sie zakonczy
+					if (aktualny == -1) {
+						tabSprawdzone.add(-1);
+					}
+				}
+			}
+		}
+		
+		//TODO Laczenie tabliczek znajdujacych sie na tym samym przystanku (przesiadki piesze)
+		//     uruchomienie algorytmu
+		
+		String info = "";
+		for (int i = 0; i < n; ++i) {
+			info += "[";
+			for (int j = 0; j < n; ++j) {
+				info += E[i][j]+",";
+			}
+			info += "]\n";
+		}
+		
+		log.info(info);
 		
 		return true;
 	}
