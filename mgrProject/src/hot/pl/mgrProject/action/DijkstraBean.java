79a9b405e53a0fcea69903cff81a5eef58eb95a1
Bartlomiej Wojas <wartek2@gmail.com>
2011-08-24 21:22:14 +0200

    Zrownoleglenie algorytmu Dijkstry

--- mgrProject/src/hot/pl/mgrProject/action/DijkstraBean.java
+++ mgrProject/src/hot/pl/mgrProject/action/DijkstraBean.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.ejb.Stateless;
 
@@ -44,12 +49,25 @@ public class DijkstraBean implements Dijkstra {
 	/**
 	 * Przechowuje sciezki tras.
 	 */
-	private int[] p;
+	private static int[] p;
 	
 	/**
 	 * Przechowuje odleglosci do wierzcholka startowego.
 	 */
-	private int[] d;
+	private static int[] d;
+	
+	/**
+	 * Liczba dostepnych procesorow. Algorytm bedzie staral sie dzialac na takiej liczbie watkow.
+	 */
+	private int nThreads;
+	
+	/**
+	 * Zwraca liczbe dostepnych procesorow (rdzeni).
+	 * @return Liczba dostepnych procesorow.
+	 */
+	private int getCores() {
+		return Runtime.getRuntime().availableProcessors();
+	}
 	
 	
 	/**
@@ -64,6 +82,8 @@ public class DijkstraBean implements Dijkstra {
 		this.E = E.clone();
 		this.V = V.clone();
 		this.s = startID;
+		int cores = getCores();
+		nThreads = cores > n ? n : cores;
 		p = new int[n]; 
 		d = new int[n];
 	}
@@ -72,6 +92,7 @@ public class DijkstraBean implements Dijkstra {
 	 * Uruchamia algorytm.
 	 */
 	public void run() {
+		log.info("Liczba watkow: " + nThreads);
 		List<Integer> S = new ArrayList<Integer>(); //wierzcholki dla ktorych zostaly policzone najkrotsze trasy
 		int u = -1;
 		List<Integer> Q = new ArrayList<Integer>(); //wierzcholki dla ktorych jeszcze nie zostaly obliczone najkrotsze trasy
@@ -84,42 +105,68 @@ public class DijkstraBean implements Dijkstra {
 		
 		d[s] = 0; //d[s-1] = 0;
 		int minD = 1000;
-		int minDindex = -1;
+		int minDIndex = -1;
 		
 		while(S.size() < n) {
+			ExecutorService exec = Executors.newFixedThreadPool(nThreads);
+			ArrayList<Future<Integer>> result = new ArrayList<Future<Integer>>();
+			log.info("d[]: " + Arrays.toString(d));
+			
 			//szukanie wierzcholka o najmniejszym d.
-			for (int i = 0; i < n; ++i) {
-				if (d[i] < minD && Q.contains(i)) { //Q.contains(i+1)
-					minD = d[i];
-					minDindex = i;
-				} else {
-					continue;
+			for (int i = 0; i < nThreads-1; ++i) {
+				result.add(exec.submit(new FindMinD(Q, i*(n/nThreads), (i+1)*(n/nThreads))));
+			}
+			result.add(exec.submit(new FindMinD(Q, (nThreads-1)*(n/nThreads), n))); //jak wyjdzie nieparzysta liczba watkow to ostatni bierze tablice do konca
+			
+			for (Future<Integer> f : result) {
+				Integer i;
+				try {
+					i = f.get();
+					if (i == -1) continue;
+					if (d[i] < minD) {
+						minD = d[i];
+						minDIndex = i;
+					}
+				} catch (InterruptedException e) {
+					log.info(e);
+				} catch (ExecutionException e) {
+					log.info(e);
+				} finally {
+					exec.shutdown();
 				}
 			}
+
 			//czasami wychodzi tu poza tablice. Prawdopodobnie wtedy, gdy graf jest niepoprawny.
 			try {
-				u = Q.get(minDindex);
+				u = Q.get(minDIndex);
 			} catch(ArrayIndexOutOfBoundsException e) {
 				throw e;
 			}
-			Q.set(minDindex, 1000);
+			Q.set(minDIndex, 1000);
 			S.add(u);
 			
 			//sprawdzanie sasiadow analizowanego wierzcholka.
-			int neighbor = -1;
+//			int neighbor = -1;
+//			
+//			for (int i = 0; i < n; ++i) {
+//				neighbor = E[u][i]; //E[u-1][i]
+//				if (neighbor != inf) {
+//					if (d[u] + neighbor < d[i]) { //d[u-1]
+//						p[i] = u;
+//						d[i] = d[u] + neighbor; //d[u-1]
+//					}
+//				}
+//			}
 			
-			for (int i = 0; i < n; ++i) {
-				neighbor = E[u][i]; //E[u-1][i]
-				if (neighbor != inf) {
-					if (d[u] + neighbor < d[i]) { //d[u-1]
-						p[i] = u;
-						d[i] = d[u] + neighbor; //d[u-1]
-					}
-				}
+			exec = Executors.newFixedThreadPool(nThreads);
+			
+			for (int i = 0; i < nThreads-1; ++i) {
+				exec.execute(new CheckNeighbors(u, E, i*(n/nThreads), (i+1)*(n/nThreads)));
 			}
+			exec.execute(new CheckNeighbors(u, E, (nThreads-1)*(n/nThreads), n));
 			
 			minD = 1000;
-			minDindex = -1;
+			minDIndex = -1;
 		}
 	}
 	
@@ -165,5 +212,68 @@ public class DijkstraBean implements Dijkstra {
 		tab.add(s);
 		
 		return tab;
+	}
+	
+	private class FindMinD implements Callable<Integer> {
+		private int start;
+		private int stop;
+		private List<Integer> Q;
+		
+		public FindMinD(List<Integer> Q, int start, int stop) {
+			this.start = start;
+			this.stop = stop;
+			this.Q = Q;
+		}
+
+		@Override
+		public Integer call() throws Exception {
+			int minD = 1001; //zeby bylo troche wieksze od zdefiniowanej nieskonczonosci
+			int minDIndex = -1;
+			
+			for (int i = start; i < stop; ++i) {
+				if (d[i] < minD && Q.contains(i)) { //Q.contains(i+1)
+					minD = d[i];
+					minDIndex = i;
+				}
+			}
+			log.info("========== start: " + start + ", stop: " + stop + ", index: " + minDIndex);
+			return minDIndex;
+		}
+	}
+	
+	private class CheckNeighbors implements Runnable {
+		private int[][] E;
+		private int start;
+		private int stop;
+		private int u;
+		
+		public CheckNeighbors(int u, int[][] E, int start, int stop) {
+			this.E = E;
+			this.start = start;
+			this.stop = stop;
+			this.u = u;
+		}
+
+		@Override
+		public void run() {
+			int neighbor = -1;
+			
+			for (int i = start; i < stop; ++i) {
+				neighbor = E[u][i]; //E[u-1][i]
+				if (neighbor != inf) {
+					if (d[u] + neighbor < d[i]) { //d[u-1]
+						p[i] = u;
+						d[i] = d[u] + neighbor; //d[u-1]
+					}
+				}
+			}
+			
+//			ArrayList<int[]> result = new ArrayList<int[]>();
+//			result.add(Arrays.copyOfRange(p, start, stop-1));
+//			result.add(Arrays.copyOfRange(d, start, stop-1));
+			
+//			return result;
+		}
+		
 	}
 }
